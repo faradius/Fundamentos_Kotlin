ea659afe955fd3fa457dcaf19f5f1d8d0ecb467f
Faradius <faradius.magic187@gmail.com>
2022-07-02 12:04:40 -0500

    Commit Inicial

--- app/src/main/java/com/pdhn/introduccionkotlin/MainActivity.kt
+++ app/src/main/java/com/pdhn/introduccionkotlin/MainActivity.kt
@@ -78,7 +78,7 @@ class MainActivity : AppCompatActivity() {
                 a/b //debe llevar un valor como las lambdas
             }catch (e: Exception){
                 println("Vamos a manejar este error")
-                "División no permitidas"
+                "División no permitida"
             }
         return res
     }
